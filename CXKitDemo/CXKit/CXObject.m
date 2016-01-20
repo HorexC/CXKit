9304f717d01ac6eb4843da2a94edcd61cd198534
chenxiang <chenxiang@noah-fund.com>
2016-01-20 14:46:31 +0800

    init code

---
+++ CXKitDemo/CXKit/CXObject.m
@@ -0,0 +1,13 @@
+//
+//  CXObject.m
+//  CXKitDemo
+//
+//  Created by Horex on 16/1/20.
+//  Copyright © 2016年 Horex. All rights reserved.
+//
+
+#import "CXObject.h"
+
+@implementation CXObject
+
+@end
